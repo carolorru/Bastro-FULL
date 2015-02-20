7e4b6df135870e36928c822aad66f4c5efeba4eb
Caroline Orrù <contato@carolineorru.com.br>
2015-02-20 15:31:29 -0200

    insert preloader assets

--- js/custom.js
+++ js/custom.js
@@ -1,128 +1,145 @@
 $(document).ready(function () {
-    // BACKGROUND - HOME
-    var i = 0
-    setInterval(function() {
-      $('.stars-front').css('background-position-x', i+'px');
-      i++;
-    }, 600);
-
-    var y = 0
-    setInterval(function() {
-      $('.stars-back').css('background-position-x', y+'px');
-      y++;
-    }, 350);
-
-    var x = 0
-    setInterval(function() {
-      $('#home').css('background-position-x', x+'px');
-      x--;
-    }, 300);
-
-
-
-    // FORM
-    // Input mask
-    $("#ddd").mask("(00)");
-    $("#telefone").mask("0000-00009");
-    // Submit form to send the data to email
-    $("#formRegister").submit(function (e) {
-        e.preventDefault();
-        if (!validateForm())
-            return false;
-
-        var dataS = {
-            "nome": $('#nome').val(),
-            "email": $('#email').val(),
-            "ddd": $('#ddd').val(),
-            "telefone": $('#telefone').val(),
-            "mensagem": $('#mensagem').val()
-        };
-
-        $.ajax({
-            type: "POST",
-            url: "mail/mailRegister.aspx",
-            data: JSON.stringify(dataS),
-            dataType: "json",
-            contentType: 'application/json; charset=utf-8',
-            success: function (response) {
-                if (response == "OK") {
-                    //alert("Obrigado por se cadastrar.");
-                    $('#modal p').text('').html("Obrigado por se cadastrar.");
-                    $('#modal, .shadown').fadeIn();
-                    setTimeout(function() {
-                      $('#modal, .shadown').fadeOut();
-                    }, 1500);
-                    $('#nome').val("");
-                    $('#email').val("");
-                    $('#ddd').val(""); ;
-                    $('#telefone').val("");
-                    $('#mensagem').val("");
-                } else
-                    $('#modal p').text('').html("Ocorreu um erro ao efetuar seu cadastro: " + response);
-                    $('#modal, .shadown').fadeIn();
-                    setTimeout(function() {
-                      $('#modal, .shadown').fadeOut();
-                    }, 1500);
-            },
-            error: function (response) {
-                $('#modal p').text('').html("Ocorreu um erro ao efetuar seu cadastro: " + response);
-                $('#modal, .shadown').fadeIn();
-                setTimeout(function() {
-                  $('#modal, .shadown').fadeOut();
-                }, 1500);
-            }
-        });
-        return false;
+    // modal
+    $('.close-modal a').on('click', function(){
+        $('body').css('overflow','auto');
+        $('#modal').hide();
+        $('#modal .row.modal').removeClass("active").hide();
     });
 
+    $('#menu .category').each(function(){
+        $(this).on('click', function(){
+            $('body').css('overflow','hidden');
+            $('#modal').fadeIn();
+            $('#'+$(this).find('a').attr('attr-modal')).addClass("active").show();
+        });
+    });
+    $('.close-modal a').click();
+
+    
+
+    // btn-like
+    $('.like').on('click', function() {
+        var item = $(this).parent().attr('data-item');
+        $('[data-item='+item+']').find('.send-email').fadeIn();
+    });
+    $('.close').on('click', function() {
+        var item = $(this).parent().parent().attr('data-item');
+        $('[data-item='+item+']').find('.send-email').fadeOut();
+    });
+
+    // framework to start animation css
+    wow = new WOW({
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+    });
+    wow.init();
+
+    // // FORM
+    // // Input mask
+    // $("#ddd").mask("(00)");
+    // $("#telefone").mask("0000-00009");
+    // // Submit form to send the data to email
+    // $("#formRegister").submit(function (e) {
+    //     e.preventDefault();
+    //     if (!validateForm())
+    //         return false;
+
+    //     var dataS = {
+    //         "nome": $('#nome').val(),
+    //         "email": $('#email').val(),
+    //         "ddd": $('#ddd').val(),
+    //         "telefone": $('#telefone').val(),
+    //         "mensagem": $('#mensagem').val()
+    //     };
+
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "mail/mailRegister.aspx",
+    //         data: JSON.stringify(dataS),
+    //         dataType: "json",
+    //         contentType: 'application/json; charset=utf-8',
+    //         success: function (response) {
+    //             if (response == "OK") {
+    //                 //alert("Obrigado por se cadastrar.");
+    //                 $('#modal p').text('').html("Obrigado por se cadastrar.");
+    //                 $('#modal, .shadown').fadeIn();
+    //                 setTimeout(function() {
+    //                   $('#modal, .shadown').fadeOut();
+    //                 }, 1500);
+    //                 $('#nome').val("");
+    //                 $('#email').val("");
+    //                 $('#ddd').val(""); ;
+    //                 $('#telefone').val("");
+    //                 $('#mensagem').val("");
+    //             } else
+    //                 $('#modal p').text('').html("Ocorreu um erro ao efetuar seu cadastro: " + response);
+    //                 $('#modal, .shadown').fadeIn();
+    //                 setTimeout(function() {
+    //                   $('#modal, .shadown').fadeOut();
+    //                 }, 1500);
+    //         },
+    //         error: function (response) {
+    //             $('#modal p').text('').html("Ocorreu um erro ao efetuar seu cadastro: " + response);
+    //             $('#modal, .shadown').fadeIn();
+    //             setTimeout(function() {
+    //               $('#modal, .shadown').fadeOut();
+    //             }, 1500);
+    //         }
+    //     });
+    //     return false;
+    // });
 
 
-    // Validate form data
-    function validateForm() {
-        var emailReg = /^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,17})$/;
 
-        var nome = $('#nome').val();
-        var email = $('#email').val();
-        var ddd = $('#ddd').cleanVal();
-        var telefone = $('#telefone').cleanVal(); ;
-        var mensagem = $('#mensagem').val();
+    // // Validate form data
+    // function validateForm() {
+    //     var emailReg = /^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,17})$/;
 
-        if (nome == "" || email == "" || ddd == "" || telefone == "" || mensagem == "") {
-            //alert("Preencha todos os campos para efetuar seu cadastro");
-            $('#modal p').text('').html("Preencha todos os campos para efetuar seu cadastro");
-            $('#modal, .shadown').fadeIn();
-            setTimeout(function() {
-              $('#modal, .shadown').fadeOut();
-            }, 1500);
-            return false;
-        }
-        else if (ddd.length != 2) {
-            //alert("Preencha um DDD válido");
-            $('#modal p').text('').html("Preencha um DDD válido");
-            $('#modal, .shadown').fadeIn();
-            setTimeout(function() {
-              $('#modal, .shadown').fadeOut();
-            }, 1500);
-            return false;
-        }
-        else if (telefone.length < 8 || telefone.length > 9) {
-            //alert("Preencha um telefone válido");
-            $('#modal p').text('').html("Preencha um telefone válido");
-            $('#modal, .shadown').fadeIn();
-            setTimeout(function() {
-              $('#modal, .shadown').fadeOut();
-            }, 1500);
-            return false;
-        }
-        else if (!emailReg.test(email)) {
-            //alert("Preencha um email válido");
-            $('#modal p').text('').html("Preencha um email válido");
-            $('#modal, .shadown').fadeIn();
-            setTimeout(function() {
-              $('#modal, .shadown').fadeOut();
-            }, 1500);
-            return false;
-        }
-        return true;
-    }   
+    //     var nome = $('#nome').val();
+    //     var email = $('#email').val();
+    //     var ddd = $('#ddd').cleanVal();
+    //     var telefone = $('#telefone').cleanVal(); ;
+    //     var mensagem = $('#mensagem').val();
+
+    //     if (nome == "" || email == "" || ddd == "" || telefone == "" || mensagem == "") {
+    //         //alert("Preencha todos os campos para efetuar seu cadastro");
+    //         $('#modal p').text('').html("Preencha todos os campos para efetuar seu cadastro");
+    //         $('#modal, .shadown').fadeIn();
+    //         setTimeout(function() {
+    //           $('#modal, .shadown').fadeOut();
+    //         }, 1500);
+    //         return false;
+    //     }
+    //     else if (ddd.length != 2) {
+    //         //alert("Preencha um DDD válido");
+    //         $('#modal p').text('').html("Preencha um DDD válido");
+    //         $('#modal, .shadown').fadeIn();
+    //         setTimeout(function() {
+    //           $('#modal, .shadown').fadeOut();
+    //         }, 1500);
+    //         return false;
+    //     }
+    //     else if (telefone.length < 8 || telefone.length > 9) {
+    //         //alert("Preencha um telefone válido");
+    //         $('#modal p').text('').html("Preencha um telefone válido");
+    //         $('#modal, .shadown').fadeIn();
+    //         setTimeout(function() {
+    //           $('#modal, .shadown').fadeOut();
+    //         }, 1500);
+    //         return false;
+    //     }
+    //     else if (!emailReg.test(email)) {
+    //         //alert("Preencha um email válido");
+    //         $('#modal p').text('').html("Preencha um email válido");
+    //         $('#modal, .shadown').fadeIn();
+    //         setTimeout(function() {
+    //           $('#modal, .shadown').fadeOut();
+    //         }, 1500);
+    //         return false;
+    //     }
+    //     return true;
+    // }   
 });
--- js/maps.js
+++ js/maps.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
    var map;
    var directionsDisplay; // Instanciaremos ele mais tarde, que será o nosso google.maps.DirectionsRenderer
    var directionsService = new google.maps.DirectionsService();
-   var markerA = new google.maps.MarkerImage('img/general/pin.png');
-   var markerB = new google.maps.MarkerImage('img/general/pin.png');
+   var markerA = new google.maps.MarkerImage('img/pin.png');
+   var markerB = new google.maps.MarkerImage('img/pin.png');
    var markersArray = [];
 
 
    function initialize() {
       directionsDisplay = new google.maps.DirectionsRenderer();
-      var myLatlng = new google.maps.LatLng(-23.5986925,-46.6900781);
+      var myLatlng = new google.maps.LatLng(-23.5836528,-46.6712752);
       var mapOptions = {
          zoom: 16,
          center: myLatlng,
@@ -19,11 +19,11 @@ $(document).ready(function () {
       }
 
       map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-      var image = 'img/general/pin.png';
+      var image = 'img/pin.png';
       var marker = new google.maps.Marker({
          position: myLatlng,
          map: map,
-         title: 'Berrini One',
+         title: 'Bastrô',
          icon: image
       });
       markersArray.push(marker);
@@ -32,45 +32,4 @@ $(document).ready(function () {
       }
       google.maps.event.addDomListener(window, 'load', initialize);
     
-   $(".form-02").submit(function(event) {
-      event.preventDefault();
-      clearOverlays();
-    
-      var enderecoPartida = $("#txtEnderecoPartida").val();
-      var enderecoChegada = 'AV. ENGENHEIRO LUÍS CARLOS BERRINI,105, SÃO PAULO';
-    
-      var request = { // Novo objeto google.maps.DirectionsRequest, contendo:
-         origin: enderecoPartida, // origem
-         destination: enderecoChegada, // destino
-         provideRouteAlternatives: false, 
-         travelMode: google.maps.TravelMode.DRIVING // meio de transporte, nesse caso, de carro
-      };
-    
-      directionsService.route(request, function(response, status) {
-         if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-            var _route = response.routes[0].legs[0]; 
-            
-            pinA = new google.maps.Marker({
-               position: _route.start_location,
-               map: map
-            }),
-
-            pinB = new google.maps.Marker({
-               position: _route.end_location,
-               map: map,
-               icon: markerB
-            });
-         }
-         markersArray.push(pinA);
-         markersArray.push(pinB);
-      });
-   });
-
-   function clearOverlays() {
-      for (var i = 0; i < markersArray.length; i++ ) {
-         markersArray[i].setMap(null);
-      }
-      markersArray.length = 0;
-   }
 });
